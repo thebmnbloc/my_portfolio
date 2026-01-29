0ebced5b3ea520144213c4d1bf3805e0563d2b3f
thebmnbloc <bioralak46@gmail.com>
2026-01-29 13:09:04 +0200

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
+  title: "biormalual.dev",
   description: "Full Stack Developer",
 };
 
